15fefd3823ed714c4d6f546b8543cd9e147aa413
harshitagaur01 <165364692+harshitagaur01@users.noreply.github.com>
2024-04-03 17:43:48 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,12 @@
+function navclick(){
+var navclick = document.getElementById("navafter");
+if(navclick.style.display=="none"){
+navclick.style.display="block";
+}
+else{
+    navclick.style.display="none"
+}
+}
+
+
+
